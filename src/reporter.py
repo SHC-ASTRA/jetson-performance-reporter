087b8adc3bbb55e9d45f306e73eae6fb36bfd0e9
Areeb Mohammed <areebmohammed11@gmail.com>
2020-10-28 19:54:32 -0500

    Initial commit

---
+++ src/reporter.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import psutil
+import rospy
+import time
+from jetson_performance_reporter.msg import PerformanceReport
+
+
+class PerformancePublisherHandler:
+	def __init__(self):
+		rospy.loginfo('Setting up node.')
+		rospy.init_node('jetson_performance_report')
+		self.report = PerformanceReport()
+		self.publisher = rospy.Publisher('/jetson_performance_report', PerformanceReport, queue_size=1)
+		rospy.loginfo('Node setup correctly!')
+
+	def publish_report(self):
+		self.report.cpu_usage = psutil.cpu_percent()
+		self.report.mem_usage = psutil.virtual_memory().percent
+		self.report.mem_total = psutil.virtual_memory().total
+		self.publisher.publish(self.report)
+
+	def run(self):
+		# set thee control rate
+		rate = rospy.Rate(2)
+		while not rospy.is_shutdown():
+			self.publish_report()
+			rate.sleep()
+
+
+if __name__ == '__main__':
+	handler = PerformancePublisherHandler()
+	handler.run()
